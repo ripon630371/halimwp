9f907c2b45b7ae8796d002a33736e3b270a2ce5d
ripon630371 <riponcmt630371@gmail.com>
2021-10-06 09:17:12 +0600

    Menu Register ok

--- functions.php
+++ functions.php
@@ -1,5 +1,19 @@
 <?php
 
+//Halim Theme Setup fucntion hock here
+function halim_setup_theme(){
+    add_theme_support('title-tag');
+    load_theme_textdomain('halim', get_template_directory_uri().'/languages');
+
+    //Rgister menu here 
+    register_nav_menus(array(
+        'main-menu' => __('Primary Menu', 'halim')
+    ));
+}
+add_action('after_setup_theme','halim_setup_theme');
+
+
+
 function halim_css_js_enqueue(){
 
     // CSS Load
--- header.php
+++ header.php
@@ -4,8 +4,6 @@
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <!-- Title -->
-      <title>Halim | Onepage Multipurpose Website</title>
       <?php wp_head();?>
    </head>
    <body>
@@ -40,16 +38,14 @@
                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                      <span class="navbar-toggler-icon"></span>
                      </button>
-                     <div class="collapse navbar-collapse ml-auto mainmenu" id="navbarNav">
-                        <ul class="navbar-nav ml-auto">
-                           <li><a href="index.html">Home</a></li>
-                           <li><a href="about.html">About</a></li>
-                           <li><a href="services.html">Services</a></li>
-                           <li><a href="portfolio.html">Portfolio</a></li>
-                           <li><a href="gallery.html">Gallery</a></li>
-                           <li><a href="blog.html">Blog</a></li>
-                           <li><a href="contact.html">Contact</a></li>
-                        </ul>
+                     <div class="collapse navbar-collapse ml-auto mainmenu justify-content-end" id="navbarNav">
+                        
+                        <?php 
+                           wp_nav_menu(array(
+                              'theme_loaction'  => 'main-menu',
+                              'menu_class' => 'navbar-nav ml-auto'
+                           ));
+                        ?>
                      </div>
                   </nav>
                </div>
